1b12aaf902ace3bb6d17cfccce612daef4fab296
Samapan Bhattacharya <sambhattacharya440@gmail.com>
2015-06-25 00:55:33 +0530

    Update fii_scrap_data.py

--- fii_scrap_data.py
+++ fii_scrap_data.py
@@ -11,11 +11,11 @@ d = msd.connect("localhost","root","sam123","fii_data" )
 cur = d.cursor()
 
 from datetime import date
-y=date.today().year
-m=date.today().month
+y=2012
+m=1
 insert_row_count=0
-
-while(y>2011):
+print "This will take several minutes...."
+while(y<date.today().year) or (m<=date.today().month):
     if(m<10):
         url= "http://www.moneycontrol.com/stocks/marketstats/fii_dii_activity/index.php?sel_month="+str(y)+"0"+str(m)
     else:
@@ -23,6 +23,9 @@ while(y>2011):
         
     r = requests.get(url)
     td=[]
+    dt_arr_temp=[]
+    gr_pur_arr_temp=[]
+    gr_sal_arr_temp=[]
     dt_arr=[]
     gr_pur_arr=[]
     gr_sal_arr=[]
@@ -32,24 +35,31 @@ while(y>2011):
         td=(row.text).split('-')
         if(td[0].isdigit()):
             t=row.text
-            dt_arr.append(t)
+            #print(t)
+            dt_arr_temp.append(t)
             count+=1
+    for i in reversed(dt_arr_temp):
+        dt_arr.append(i)
         
     c=0        
     for row in BeautifulSoup(r.content).findAll('td',{"width":"102"}):
         if(c<count):
-            gr_pur_arr.append(row.text)
+            gr_pur_arr_temp.append(row.text)
             c+=1
         else:
             break
-                
+    for i in reversed(gr_pur_arr_temp):
+        gr_pur_arr.append(i)
+            
     c=0        
     for row in BeautifulSoup(r.content).findAll('td',{"width":"83"}):
         if(c<count):
-            gr_sal_arr.append(row.text)
+            gr_sal_arr_temp.append(row.text)
             c+=1
         else:
             break
+    for i in reversed(gr_sal_arr_temp):
+        gr_sal_arr.append(i)     
         
     for x in xrange(0,count):
         dt=dt_arr[x].encode('ascii','ignore')
@@ -60,9 +70,10 @@ while(y>2011):
             cur.execute("insert into records(date,gross_purchase,gross_sales)values(%s,%s,%s)",(dt,purchase,sales))
             d.commit()
             insert_row_count+=1
-    if(m==1):
-        m=12
-        y-=1
+    if(m==12):
+        m=1
+        y+=1
     else:
-        m-=1
+        m+=1
 print insert_row_count,"rows inserted"    
+    
